8a405c9deef11acc0e06e90fe5200a01a1ea6f36
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-02-03 13:55:35 +0530

    [Saurabh'Add CalculatingWagesForMonths

--- Utility.js
+++ Utility.js
@@ -5,27 +5,40 @@ var empHrs = 0;
 class Utility {
 
     isPresentorAbsent() {
-        console.log("Welcome to the Employee Wage");
+        
         let empCheck = (Math.floor(Math.random() * 10) % 3);
         switch (empCheck) {
             case IS_FULL_TIME:
                 empHrs = 8;
-                console.log("Employee is Full Time");
                 break;
             case IS_PART_TIME:
                 empHrs = 4;
-                console.log("Employee is Half Time");
                 break;
             default:
                 empHrs=0;
-                console.log("Employee is Absent");
         }
           return empHrs;
     }
+
     calculateEmpWage() {
         const Wage_Per_Hour = 20;
         let workHours = Wage_Per_Hour * this.isPresentorAbsent();
-        console.log("Employee Wage is: " + workHours);
+        return workHours;
+
+    }
+
+    monthWageCalculate()
+    {
+        console.log("Welcome to the Employee Wage");
+        let workday_in_month=20;
+        let totalWork=0;
+        for(let i=0;i <=workday_in_month; i++)
+        {   
+            totalWork=totalWork + this.calculateEmpWage();    
+        }
+        console.log("Total work is done: "+ totalWork);
     }
 }
+
+
 module.exports = new Utility();
