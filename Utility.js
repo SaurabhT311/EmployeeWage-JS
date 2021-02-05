c45b35da11ec12050a3c6d71c3d407d799d41f5c
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-02-05 11:06:39 +0530

    [Saurabh]Modify AddedEmployeeExtendedData

--- Utility.js
+++ Utility.js
@@ -1,9 +1,11 @@
 class EmployeePayroll {
 
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this._id = id;
         this._name = name;
         this._salary = salary;
+        this._gender= gender;
+        this._startDate=startDate;
     }
     get name() {
         return this._name;
@@ -27,9 +29,31 @@ class EmployeePayroll {
     get salary() {
         return this._salary;
     }
+
+    set gender(gender)
+    {
+         this._gender=gender;
+    }
+
+    get gender()
+    {
+        return this._gender;
+    }
+
+    set startDate(date)
+    {
+        this._startDate=startDate;
+    }
+
+    get startDate()
+    {
+        return this._startDate;
+    }
 }
 
-let emp = new EmployeePayroll(7, "Saurabh", 25000);
-console.log("Employee name is :" + emp._name);
-console.log("Employee id is :" + emp._id);
-console.log("Employee salary is :" + emp._salary);
+let emp = new EmployeePayroll(7, "Saurabh", 25000, 'Male', "20 Nov");
+console.log("Employee name is :" + emp._name );
+ console.log("Employee id is :" + emp._id);
+ console.log("Employee salary is :" + emp._salary);
+console.log("Employee gender is: "+ emp._gender);
+console.log("Employee date of joining: "+ emp._startDate);
